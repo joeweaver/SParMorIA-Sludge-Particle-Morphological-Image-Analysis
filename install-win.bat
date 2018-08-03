f94211cbe37ce73098734fb1ea47a2cb7710d303
Joe Weaver <jeweave4@ncsu.edu>
2018-08-03 10:09:55 -0400

    Add simple installation batch file

---
+++ install-win.bat
@@ -0,0 +1,6 @@
+REM replace <macro dir here> with the path to your local fiji macro director
+REM Example:
+REM set macro_dir=%USERPROFILE%\Dropbox\RoamingApps\fiji-win64\Fiji.app\macros
+
+set macro_dir=<macro dir here>
+copy SParMorIA-Sludge-Particle_Morphological_Image_Analysis.ijm %macro_dir%
